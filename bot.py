72acc2c63d64b8de55b2db259540ef887bd8afcf
EMCain <>
2018-06-16 16:46:57 -0700

    get credentials from environment

--- bot.py
+++ bot.py
@@ -2,7 +2,16 @@
 
 import tweepy, time, sys 
 
-from credentials import *
+# from credentials import *  # use this one for testing 
+
+# use this for production; set vars in heroku dashboard
+from os import environ 
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
+
 from generate_advertisement import get_ad
 
 # INTERVAL = 60 * 60 * 6  # tweet every 6 hours
