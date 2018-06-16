2b1fcd279a3256df56cbf46c47a31eca6bbeee6c
EMCain <>
2018-06-16 16:50:40 -0700

    change interval back to 6 hrs

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ ACCESS_SECRET = environ['ACCESS_SECRET']
 
 from generate_advertisement import get_ad
 
-# INTERVAL = 60 * 60 * 6  # tweet every 6 hours
-INTERVAL = 15  # every 15 seconds, for testing
+INTERVAL = 60 * 60 * 6  # tweet every 6 hours
+# INTERVAL = 15  # every 15 seconds, for testing
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
